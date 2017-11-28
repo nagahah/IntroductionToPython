218e8853f624bd393e9cdf087a9044b7ea578ec6
nagahah <nagahah@rose-hulman.edu>
2017-11-28 16:25:59 -0500

    Finish

--- src/m5_your_turtles.py
+++ src/m5_your_turtles.py
@@ -2,10 +2,10 @@
 Your chance to explore Loops and Turtles!
 
 Authors: David Mutchler, Dave Fisher, Valerie Galluzzi, Amanda Stouder,
-         their colleagues and PUT_YOUR_NAME_HERE.
+         their colleagues and HIROAKI.
 """
 ########################################################################
-# TODO: 1.
+# DONE: 1.
 # On Line 5 above, replace  PUT_YOUR_NAME_HERE  with your own name.
 ########################################################################
 
@@ -28,3 +28,30 @@ Authors: David Mutchler, Dave Fisher, Valerie Galluzzi, Amanda Stouder,
 #
 #  Don't forget to COMMIT your work by using  VCS ~ Commit and Push.
 ########################################################################
+import rosegraphics as rg
+window = rg.TurtleWindow()
+red_turtle = rg.SimpleTurtle('turtle')
+red_turtle.pen = rg.Pen('red', 5)
+red_turtle.speed = 5
+size=200
+for j in range(5):
+    red_turtle.right(50)
+    red_turtle.forward(100)
+    red_turtle.left(70)
+
+
+
+import rosegraphics as rg
+window = rg.TurtleWindow()
+black_turtle = rg.SimpleTurtle('turtle')
+black_turtle.pen = rg.Pen('black', 3)
+black_turtle.speed = 10
+size=150
+for j in range(7):
+    black_turtle.right(100)
+    black_turtle.forward(70)
+    black_turtle.left(50)
+
+window.close_on_mouse_click()
+
+
