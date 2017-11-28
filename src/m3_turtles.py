bce62dc800461ec039b99576d8bf825c309843ee
nagahah <nagahah@rose-hulman.edu>
2017-11-28 15:47:16 -0500

    Finish

--- src/m3_turtles.py
+++ src/m3_turtles.py
@@ -10,11 +10,11 @@ Also:
   -- ASSIGNING a VALUE to a NAME (VARIABLE).
 
 Authors: David Mutchler, Dave Fisher, Valerie Galluzzi, Amanda Stouder,
-         their colleagues and PUT_YOUR_NAME_HERE.
+         their colleagues and HIROAKI.
 """
 ########################################################################
 #
-# TODO: 1.
+# DONE: 1.
 #  (Yes, that means for YOU to DO things per these instructions:)
 #
 # On Line 13 above, replace  PUT_YOUR_OWN_NAME_HERE  with your OWN name.
@@ -29,7 +29,7 @@ import rosegraphics as rg
 
 ########################################################################
 #
-# TODO: 2.
+# DONE: 2.
 #   Allow this file to use the rosegraphics.py file by marking the src
 #   directory as a "Sources Root".  Do that by right clicking on the src folder,
 #   then selector  Mark Directory As --> Sources Root
@@ -70,14 +70,14 @@ dave.forward(200)
 matt = rg.SimpleTurtle('turtle')
 matt.pen = rg.Pen('red', 30)
 matt.speed = 10  # Faster
-matt.backward(50)
-matt.left(90)
-matt.forward(50)
+matt.backward(80)
+matt.left(150)
+matt.forward(70)
 
 
 ########################################################################
 #
-# TODO: 3.
+# DONE: 3.
 #   Add a few more line of your own code above to make one of the
 #   existing Turtles move some more and/or have different
 #   characteristics.
@@ -91,7 +91,7 @@ matt.forward(50)
 
 ########################################################################
 #
-# TODO: 4.
+# DONE: 4.
 #   The code above  CONSTRUCTS  two SimpleTurtle objects and gives those objects NAMES:
 #       dave    matt
 #
@@ -109,10 +109,16 @@ matt.forward(50)
 #   As always, test by running the module.
 #
 ########################################################################
-
+matt = rg.SimpleTurtle('turtle')
+matt.pen = rg.Pen('blue', 30)
+matt.speed = 10  # Faster
+matt.backward(210)
+matt.left(320)
+matt.forward(300)
+matt.right(200)
 ########################################################################
 #
-# TODO: 5.
+# DONE: 5.
 #   Run one more time to be sure that all is still OK.
 #   Ensure that no blue bars on the scrollbar-thing to the right remain.
 #
